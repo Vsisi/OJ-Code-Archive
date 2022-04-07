58e88031f4ef9a1811ef65d2225e510f23f5e252
Vsisi <67737678+Vsisi@users.noreply.github.com>
2022-04-07 18:11:21 +0800

    Create 4003.cpp

---
+++ 4003.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    int n;
+    while(t--) {
+        cin >> hex >> n;
+        cout << n << endl;
+    }
+    return 0;
+}
